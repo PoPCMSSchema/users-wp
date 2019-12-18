2e76cdbf8a29423801d11ad4abbb98cb17eca2b4
Leo <losoviz@gmail.com>
2019-12-18 13:33:11 +0800

    Added TypeAPI

---
+++ src/TypeAPIs/UserTypeAPI.php
@@ -0,0 +1,21 @@
+<?php
+namespace PoP\UsersWP\TypeAPIs;
+
+use WP_User;
+use PoP\Users\TypeAPIs\UserTypeAPIInterface;
+/**
+ * Methods to interact with the Type, to be implemented by the underlying CMS
+ */
+class UserTypeAPI implements UserTypeAPIInterface
+{
+    /**
+     * Indicates if the passed object is of type User
+     *
+     * @param [type] $object
+     * @return boolean
+     */
+    public function isInstanceOfUserType($object): bool
+    {
+        return $object instanceof WP_User;
+    }
+}
